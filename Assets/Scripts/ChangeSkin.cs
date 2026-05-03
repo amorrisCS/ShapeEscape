49008207bb5ed8544677a83dafb3843dd9dfc8db
Arianna <arianna.cooper.business1@gmail.com>
2026-05-03 17:35:22 -0500

    update main menu screen with changing character and loading features

--- Assets/Scripts/ChangeSkin.cs
+++ Assets/Scripts/ChangeSkin.cs
@@ -2,26 +2,13 @@ using UnityEngine;
 
 public class ChangeSkin : MonoBehaviour
 {
-     private SpriteRenderer sr;
-
-    void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
-
     void Start()
-    {
-        ApplySkin();
-    }
-
-    public void ApplySkin()
     {
         if (SkinManager.Instance != null)
         {
-            // pulls selected sprite from skin manager options
-            sr.sprite = SkinManager.Instance.GetSelectedSprite();
-            // changes green hue to white 
-            sr.color = Color.white;
+            // to change skin instance to what was selected in main menu
+            GetComponent<SpriteRenderer>().sprite = SkinManager.Instance.skins[SkinManager.Instance.selected];
+            GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
 }
--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -2,30 +2,14 @@ using UnityEngine;
 
 public class SkinManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public static SkinManager Instance {get; private set;}
-
-    [Header("Skins Swap")]
+    public static SkinManager Instance;
     public Sprite[] skins;
-    public string[] skinNames;
+    public int selected = 0;
 
-    public int SelectedIndex { get; private set;} = 0;
-
-    void Awake(){
-        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+    void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        SelectedIndex = PlayerPrefs.GetInt("SelectedSkin", 0);
     }
-
-     public void SelectSkin(int index)
-    {
-        SelectedIndex = index;
-        PlayerPrefs.SetInt("SelectedSkin", index);
-    }
-
-    public Sprite GetSelectedSprite() => skins[SelectedIndex];
-    public string GetSelectedName()   => skinNames[SelectedIndex];
 }
-
-
