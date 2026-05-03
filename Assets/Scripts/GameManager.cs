52db3df36afea49ff1da2b16ce0388c4b5c06e39
Arianna <arianna.cooper.business1@gmail.com>
2026-05-02 21:31:56 -0500

    changes pushed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,4 +24,6 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    
 }
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Goal : MonoBehaviour
+{
+    static public bool escaped = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    { 
+        if (other.tag == "Player")
+        {
+            Goal.escaped = true;
+            SceneManager.LoadScene("SampleScene");
+            Material mat = GetComponent<Renderer>().material;
+            Color color = mat.color;
+            color.a = 0.5f;
+            mat.color = color;
+        }
+    }
+
+}
